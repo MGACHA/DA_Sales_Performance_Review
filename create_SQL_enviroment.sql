7bffffb4dc55fdd3eb11b3ce804cb36dd04859f6
MGACHA <71405661+MGACHA@users.noreply.github.com>
2026-04-30 12:06:48 +0100

    Add SQL script for DA_Sales environment setup
    Added SQL script for creating database and schemas.

---
+++ create_SQL_enviroment.sql
@@ -0,0 +1,12 @@
+--CREATE DATABASE DA_Sales;
+--USE DA_Sales;
+
+-- Create schemas
+--CREATE SCHEMA staging;
+--CREATE SCHEMA warehouse;
+--CREATE SCHEMA reporting;
+--GO
+
+/*SELECT * 
+FROM sys.schemas
+ORDER BY name; */
